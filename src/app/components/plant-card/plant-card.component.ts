47bd8d2cde94cea8537af124092ebd3a1460e5fd
Uttam-K-Roy <uttam.roy.ext@holcim.com>
2024-02-12 16:24:27 +0600

    plant functional card Done

--- src/app/components/plant-card/plant-card.component.ts
+++ src/app/components/plant-card/plant-card.component.ts
@@ -1,75 +1,28 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { PlantsModel } from "src/app/store/models/plant.model";
 import {
-  IonRow,
   IonCol,
+  IonRow,
   IonGrid,
   IonIcon,
   IonButton,
 } from "@ionic/angular/standalone";
-import {
-  Component,
-  Input,
-  OnInit,
-  WritableSignal,
-  input,
-  signal,
-} from "@angular/core";
-import { PlantsModel } from "src/app/store/models/plant.model";
-import { DatePipe } from "@angular/common";
 
 @Component({
   standalone: true,
   selector: "app-plant-card",
   templateUrl: "./plant-card.component.html",
   styleUrls: ["./plant-card.component.scss"],
-  imports: [IonButton, IonGrid, IonRow, IonIcon, IonCol, DatePipe],
+  imports: [IonCol, IonRow, IonButton, IonGrid, IonIcon],
 })
 export class PlantCardComponent implements OnInit {
   @Input() plant: PlantsModel;
 
-  isApprovedAssets: boolean = false;
-  isRejectedAssets: boolean = false;
-  isMappedAssets: boolean = false;
-
-  // isAssetZero: boolean = false;
-
   constructor() {
     this.plant = {};
   }
-
   ngOnInit() {
     console.log(this.plant);
-
-    if (this.plant.totalApprovedAssets != 0) {
-      this.isApprovedAssets = true;
-    }
-
-    if (this.plant.totalRejectedAssets != 0) {
-      this.isRejectedAssets = true;
-    }
-
-    if (this.plant.totalMappedAssets != 0) {
-      this.isMappedAssets = true;
-    }
-
-    // this.plant.values.forEach((i: any) => {
-    //   this.assetValues.push(i);
-    // });
-
-    // this.assetValues.forEach((asset) => {
-    //   if (plant.totalMappedAssets === "Asset Approved") {
-    //     asset.bgColor = "bg-green-600";
-    //   }
-    //   if (asset.status === "Asset Rejected By Approver") {
-    //     asset.bgColor = "bg-rose-500";
-    //   }
-    //   if (asset.status === "Asset Registeration Pending") {
-    //     asset.bgColor = "bg-orange-500";
-    //   }
-    //   if (asset.status === "Asset Approval Pending") {
-    //     asset.bgColor = "bg-amber-400";
-    //   }
-    // });
   }
-
   handleMenuToggle = () => {};
 }
