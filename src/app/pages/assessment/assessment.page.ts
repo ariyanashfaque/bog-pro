eb4c0a1d1f0d25390609424e037d54191a63c40e
Nazmul Kabir <nazmul.kabir.ext@holcim.com>
2024-02-27 15:37:57 +0600

    assessment modal trying in assessment page

--- src/app/pages/assessment/assessment.page.ts
+++ src/app/pages/assessment/assessment.page.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { IonicModule } from "@ionic/angular";
+import { HeaderComponent } from "src/app/components/header/header.component";
+import { AssessmentModalComponent } from "src/app/components/assessment-modal/assessment-modal.component";
 
 @Component({
-  selector: 'app-assessment',
-  templateUrl: './assessment.page.html',
-  styleUrls: ['./assessment.page.scss'],
+  selector: "app-assessment",
+  templateUrl: "./assessment.page.html",
+  styleUrls: ["./assessment.page.scss"],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+    AssessmentModalComponent,
+  ],
 })
 export class AssessmentPage implements OnInit {
+  isAssessmentModalOpen: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  assessmentModalToggle = () => {
+    this.isAssessmentModalOpen = !this.isAssessmentModalOpen;
+  };
 }
