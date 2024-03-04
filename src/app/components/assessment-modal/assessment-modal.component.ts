071e140b68d076873b6e6d17b9c43fa986960d31
NazmulExt <nazmul.kabir.ext@holcim.com>
2024-03-04 17:58:24 +0600

    assessment modal search bar done

--- src/app/components/assessment-modal/assessment-modal.component.ts
+++ src/app/components/assessment-modal/assessment-modal.component.ts
@@ -1,12 +1,40 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { IonicModule } from "@ionic/angular";
+import {
+  IonRow,
+  IonCol,
+  IonGrid,
+  IonItem,
+  IonList,
+  IonText,
+  IonLabel,
+  IonTitle,
+  IonSelect,
+  IonTextarea,
+  IonInput,
+  IonSelectOption,
+  IonIcon,
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: "app-assessment-modal",
   standalone: true,
   templateUrl: "./assessment-modal.component.html",
   styleUrls: ["./assessment-modal.component.scss"],
-  imports: [IonicModule],
+  imports: [
+    IonIcon,
+    IonInput,
+    IonText,
+    IonRow,
+    IonCol,
+    IonGrid,
+    IonItem,
+    IonList,
+    IonLabel,
+    IonTitle,
+    IonSelect,
+    IonTextarea,
+    IonSelectOption,
+  ],
 })
 export class AssessmentModalComponent implements OnInit {
   @Input() isMenuOpen: boolean = false;
@@ -20,5 +48,14 @@ export class AssessmentModalComponent implements OnInit {
   menuToggle() {
     this.isMenuOpen = !this.isMenuOpen;
     this.isMenuOpenChange.emit(this.isMenuOpen);
+  }
+
+  authorSearch: boolean = false;
+  teamMemberSearch: boolean = false;
+  toggleAuthorSearch() {
+    this.authorSearch = !this.authorSearch;
+  }
+  toggleTeamMemberSearch() {
+    this.teamMemberSearch = !this.teamMemberSearch;
   }
 }
