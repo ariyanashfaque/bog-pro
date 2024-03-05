00c03b9a195bb41be9b189bd006b6390a6590a5d
NazmulExt <nazmul.kabir.ext@holcim.com>
2024-03-05 17:22:46 +0600

    search module done

--- src/app/components/assessment-modal/assessment-modal.component.ts
+++ src/app/components/assessment-modal/assessment-modal.component.ts
@@ -13,7 +13,11 @@ import {
   IonInput,
   IonSelectOption,
   IonIcon,
+  IonButtons,
+  IonAvatar,
+  IonButton,
 } from "@ionic/angular/standalone";
+import { idCard } from "ionicons/icons";
 
 @Component({
   selector: "app-assessment-modal",
@@ -21,6 +25,9 @@ import {
   templateUrl: "./assessment-modal.component.html",
   styleUrls: ["./assessment-modal.component.scss"],
   imports: [
+    IonButton,
+    IonAvatar,
+    IonButtons,
     IonIcon,
     IonInput,
     IonText,
@@ -57,5 +64,74 @@ export class AssessmentModalComponent implements OnInit {
   }
   toggleTeamMemberSearch() {
     this.teamMemberSearch = !this.teamMemberSearch;
+  }
+
+  // A dummy array for the select options with the user names and emails
+  users = [
+    {
+      id: 1,
+      name: "Ariyan Ashfaq",
+      email: "ariyan@holcim.com",
+      selected: false,
+    },
+    {
+      id: 2,
+      name: "Mustafa Iram",
+      email: "mustafa@gmail.com",
+      selected: false,
+    },
+    {
+      id: 3,
+      name: "M.H. Nahib",
+      email: "nahib@holcim.com",
+      selected: false,
+    },
+    {
+      id: 4,
+      name: "Sadia Tasnim",
+      email: "sadia@holcim.com",
+      selected: false,
+    },
+    {
+      id: 5,
+      name: "Uttam Roy",
+      email: "uttam@yahoo.c0m",
+      selected: false,
+    },
+    {
+      id: 6,
+      name: "Nazmul Kabir",
+      email: "kabir@holcim.com",
+      selected: false,
+    },
+    {
+      id: 7,
+      name: "Ashikul Shafol",
+      email: "shofol@holcim.com",
+    },
+    {
+      id: 8,
+      name: "Musaddi Rafi",
+      email: "rafi@gmail.com",
+    },
+  ];
+
+  // a array for the selected users from the select options
+  selectedUsers: any[] = [];
+
+  // a function to add the selected user to the selectedUsers array
+  addUser(user: any) {
+    this.selectedUsers.push(user);
+    user.selected = true;
+    console.log(this.selectedUsers);
+    console.log(this.users);
+  }
+
+  // a function to remove the selected user from the selectedUsers array
+  removeUser(user: any) {
+    this.selectedUsers = this.selectedUsers.filter(
+      (selectedUser) => selectedUser !== user,
+    );
+    user.selected = false;
   }
 }
