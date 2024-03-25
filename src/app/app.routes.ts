759b19c66f9334decf972438b1b0d8b319718174
uttamext <uttam.roy.ext@holcim.com>
2024-03-25 11:50:35 +0600

    Bulk upload page Ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
     path: "",
     loadChildren: () =>
       import("./tabs/tabs.routes").then((route) => route.routes),
+  },  {
+    path: 'bulk-upload',
+    loadComponent: () => import('./pages/asset-register/bulk-upload/bulk-upload.page').then( m => m.BulkUploadPage)
   },
+
 ];
